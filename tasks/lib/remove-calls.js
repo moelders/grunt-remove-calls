19a353edb9e8198ccc97fbcd14bd87c7100298fa
Jaime Pastor Pueyo <jppueyo@tid.es>
2014-09-16 16:36:30 +0200

    FIX reg-exp because it failed when the expression was inside a comment

--- tasks/lib/remove-calls.js
+++ tasks/lib/remove-calls.js
@@ -8,17 +8,25 @@ exports.init = function(grunt) {
   return function(src, opts) {
     var _counter = 0,
       _src = src || '',
-      _regExp;
+      _comment = '(//|/\\*|\\*)\\s*',
+      _regExp, _commentRegExp;
 
     if (('namespaces' in opts) && ('methods' in opts)) {
-      _regExp = new RegExp('^(?!/(/|\\*))\\s*(window.)?(' +
+      _regExp = new RegExp('(' + _comment + ')?(window.)?(' +
         opts.namespaces.join('|') + ')' + '.(?:' + opts.methods.join('|') +
           ')\\s{0,}\\([^;]*\\)(?!\\s*[;,]?\\s*\\/\\*\\s*remove-calls:skip\\s*' +
-          '\\*\\/)\\s{0,};?', 'gi');
+          '\\*\\/)\\s{0,};?', 'g');
+      _commentRegExp = new RegExp('^' + _comment);
 
-      _src = _src.replace(_regExp, function() {
-        _counter++;
-        return opts.replaceWith || '';
+      _src = _src.replace(_regExp, function(match) {
+        var result = match;
+
+        if (!_commentRegExp.test(match)) {
+          _counter++;
+          result = opts.replaceWith || '';
+        }
+
+        return result;
       });
     }
 
